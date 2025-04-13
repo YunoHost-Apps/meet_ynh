f91e1f3abc1db890d02c0ac30641d96714762100
fflorent <florent.git@zeteo.me>
2025-04-13 16:31:08 +0200

    Make installation work

---
+++ conf/meet.py
@@ -0,0 +1,18 @@
+# See https://github.com/suitenumerique/meet/blob/main/docker/files/usr/local/etc/gunicorn/meet.py for the list of variables
+
+# Gunicorn-django settings
+bind = ["0.0.0.0:__PORT_BACKEND__"]
+name = "meet"
+python_path = "__INSTALL_DIR__/src/backend"
+
+# Run
+graceful_timeout = 90
+timeout = 90
+workers = 3
+
+# Logging
+# Using '-' for the access log file makes gunicorn log accesses to stdout
+accesslog = "-"
+# Using '-' for the error log file makes gunicorn log errors to stderr
+errorlog = "-"
+loglevel = "info"
