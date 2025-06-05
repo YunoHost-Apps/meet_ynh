da29a4afce533cf5a4d54c6409216a9074fb4f42
fflorent <florent.git@zeteo.me>
2025-06-05 21:03:54 +0200

    Change how dependency to dex is handled

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,27 +5,34 @@
 #=================================================
 nodejs_version=22
 
-dex_ynh_commit=d04fc79d385313a11bc9982454b4a659148db77c
+setup_dex() {
+  # List the Dex apps installed on the system
+  dex_apps="$(yunohost app list -f --output-as json | jq -r '[ .apps[] | select(.manifest.id == "dex") ]')"
+  dex="${dex:-dex}"
 
-install_dex() {
-  local dex_domain_path
-  local dex_domain_path_no_trailing_slash
-
-  oidc_secret=$(ynh_string_random -l 32)
-  oidc_name="$app"
-  oidc_callback="$domain/api/v1.0/callback/"
-
-  if yunohost app list | grep -q "$dex_domain$dex_path"; then
-    ynh_die "The domain provided for Dex is already used by another app. Please choose another one!"
+  # If there are no Dex app installed
+  if [ $(jq -r '[ .[] | select(.manifest.id == "dex").id ] | length' <<< $dex_apps) -eq 0 ]
+  then
+      ynh_die "The apps needs at least one Dex instance to be installed. Install or restore one first."
+  # Else if the configured Dex app is not in the list, default to the first one and display a warning
+  elif [ $(jq --arg dex $dex -r '[ .[] | select(.id == $dex) ] | length' <<< $dex_apps) -ne 1 ]
+  then
+    dex="$(jq -r 'sort_by(.id) | first.id' <<< $dex_apps)"
+    ynh_print_warn "The dex app was not set up, or the one initially set up for $app has not been found. Reconfiguring with $dex"
+    ynh_app_setting_set --key=dex --value=$dex
   fi
 
-  yunohost app install https://github.com/YunoHost-Apps/dex_ynh/tree/$dex_ynh_commit --force --args "domain=$dex_domain&path=$dex_path&oidc_name=$oidc_name&oidc_secret=$oidc_secret&oidc_callback=$oidc_callback" 2>&1 | tee dexlog.txt
-  dex_app=$(grep -Po 'Installation of\s+\K.*(?=\s+completed)' dexlog.txt)
-  rm dexlog.txt
-
-  if [ -z "$dex_app" ]; then
-    ynh_die "Dex package installation failed"
+  # Make sure that the Dex version is compatible
+  dex_version=$(yunohost app info $dex --output-as json | jq -r '.version')
+  if [ $(dpkg --compare-versions "${dex_version#v}" lt "2.42.1~ynh4") ]; then
+    ynh_die "You need to upgrade $dex to v2.42.1~ynh4 and above first."
   fi
+
+  # Prepare the variables
+  dex_install_dir="$(ynh_app_setting_get --app $dex --key install_dir)"
+  dex_domain="$(ynh_app_setting_get --app $dex --key domain)"
+  dex_path="$(ynh_app_setting_get --app $dex --key path)"
+  oidc_callback="https://$domain${path%/}/api/v1.0/callback/"
 
   # Create Dex URIs
   dex_domain_path="${dex_domain}${dex_path}"
@@ -39,12 +46,16 @@ install_dex() {
   dex_keys_uri="https://${dex_domain_path_no_trailing_slash}/keys"
   dex_user_uri="https://${dex_domain_path_no_trailing_slash}/userinfo"
 
-  ynh_app_setting_set --key=dex_app --value=$dex_app
-  ynh_app_setting_set --key=dex_auth_uri --value=$dex_auth_uri
-  ynh_app_setting_set --key=dex_token_uri --value=$dex_token_uri
-  ynh_app_setting_set --key=dex_keys_uri --value=$dex_keys_uri
-  ynh_app_setting_set --key=dex_user_uri --value=$dex_user_uri
-  ynh_app_setting_set --key=oidc_name --value=$oidc_name
-  ynh_app_setting_set --key=oidc_secret --value=$oidc_secret
-  ynh_app_setting_set --key=oidc_callback --value=$oidc_callback
+  # Store the variables
+  ynh_app_setting_set         --key=dex_install_dir       --value="$dex_install_dir"
+  ynh_app_setting_set         --key=dex_user_uri          --value="$dex_user_uri"
+  ynh_app_setting_set         --key=dex_auth_uri          --value="$dex_auth_uri"
+  ynh_app_setting_set         --key=dex_keys_uri          --value="$dex_keys_uri"
+  ynh_app_setting_set         --key=dex_token_uri         --value="$dex_token_uri"
+  ynh_app_setting_set_default --key=oidc_name             --value="$app"
+  ynh_app_setting_set         --key=oidc_callback         --value="$oidc_callback"
+  ynh_app_setting_set_default --key=oidc_secret           --value="$(ynh_string_random --length=32 --filter='A-F0-9')"
+
+  # Add the configuration file for the app in Dex
+  bash "$dex_install_dir/add_config.sh" $app $oidc_name $oidc_callback $oidc_secret
 }
